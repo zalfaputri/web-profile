3bcf2579b009eb468fa20559ac8e8ee2f1928106
zalfaputri <110963790+zalfaputri@users.noreply.github.com>
2022-09-29 13:23:22 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+    crossorigin="anonymous">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="row" style="margin-top:50px;"> <div class="col-md"></div> <div class="col-md rounded 
+    bg-light" style="box-shadow: 4px 4px 5px -4px;padding:10px"> <form action="proses_login.php" 
+    method="post"> <h3 align="center">LOGIN Perpus Online</h3> Username: <input type="text" 
+    name="username" value="" class="form-control"> password: <input type="password" name="password" 
+    class="form-control"><br> <center><input type="submit" name="login" class="btn btn-success" 
+    value="LOGIN"></center> </form> </div> <div class="col-md"></div> </div>
+</body>
+</html>
